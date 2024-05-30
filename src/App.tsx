02ec0ce732425b83effa70ef452aeea647ff46cf
Pokellelo <pokellelo@gmail.com>
2024-05-29 19:45:13 -0600

    only on column

--- src/App.tsx
+++ src/App.tsx
@@ -12,28 +12,28 @@ export default function App() {
   const [data, setData] = useState(createItems());
   const [isLoading, setIsLoading] = useState(false);
 
-  const storedLan:string | null = localStorage.getItem('language')
+  const storedLan: string | null = localStorage.getItem('language')
 
 
 
-  const lanSelected:Language = storedLan ? Language[Language[parseInt(storedLan)] as keyof typeof Language] : Language.English
+  const lanSelected: Language = storedLan ? Language[Language[parseInt(storedLan)] as keyof typeof Language] : Language.English
   const mn = months[lanSelected]
-  
+
 
   const [year, setYear] = useState(new Date().getFullYear() + 1);
 
   window.addEventListener("wheel", e => {
     let list = document.getElementById("calendar-container")
 
-    if(list){
-      e.deltaY === 100 ?  list.scrollLeft += 5 : list.scrollLeft -= 5
+    if (list) {
+      e.deltaY === 100 ? list.scrollLeft += 5 : list.scrollLeft -= 5
     }
   });
-  
-  const next = async (direction: ScrollDirection) => { 
+
+  const next = async (direction: ScrollDirection) => {
     try {
 
-      setYear(year+1);
+      setYear(year + 1);
 
       setIsLoading(true);
       const newData = await loadMore(year);
@@ -49,34 +49,35 @@ export default function App() {
     }
   };
 
-    const ref = useInfiniteScroll<HTMLInputElement>({
-      next,
-      columnCount: data.length,
-      hasMore: { right: true },
-      scrollThreshold: 0.1
+  const ref = useInfiniteScroll<HTMLInputElement>({
+    next,
+    columnCount: data.length,
+    hasMore: { right: true },
+    scrollThreshold: 0.1
 
-    });
-  
+  });
+
   return (
 
-    <div id="calendar-container" ref={ref}>
-      {data.map((v, i)=> (
-        <div key={i+"-"+v.year} >
+    <div id="calendar-container" ref={ref} className="flex">
+      {data.map((v, i) => (
+        <div key={i + "-" + v.year} >
           <div>
             {v.year}
           </div>
-          
-          <div className="month">
+
+          <div className="month flex">
             {v.months.map((days, month) => (
               <div key={month + "-month"}>
                 <b className="month-name">{mn[month]}</b>
                 <div className="day-container">
                   {days.map((day, id_day) => (
                     <div className="day" key={id_day}>
-                    
-                      <div>{day}</div>
-                      <span>{Math.floor(Math.random()*6) > 3 ? <div className="event">Evnt</div> : <div className=""> +</div>}</span>
-                      <span>{Math.floor(Math.random()*6) > 3 ? <div className="event">Evnt</div> : <div className=""> +</div>}</span>
+
+                      <div>{}<br></br>
+                        {day}</div>
+                      <span>{Math.floor(Math.random() * 6) > 3 ? <div className="event">Evnt</div> : <div className=""> +</div>}</span>
+                      <span>{Math.floor(Math.random() * 6) > 3 ? <div className="event">Evnt</div> : <div className=""> +</div>}</span>
 
                     </div>
                   ))}
--- src/lib/components/Header.tsx
+++ src/lib/components/Header.tsx
@@ -1,13 +1,13 @@
 import { Language } from "../enums"
-import { miselanea} from "../dictionaries"
+import { miselanea } from "../dictionaries"
 import { useState } from "react"
 
-const storedLan:string | null = localStorage.getItem('language')
+const storedLan: string | null = localStorage.getItem('language')
 
-let lanSelected:Language = storedLan ? Language[Language[parseInt(storedLan)] as keyof typeof Language] : Language.English
+let lanSelected: Language = storedLan ? Language[Language[parseInt(storedLan)] as keyof typeof Language] : Language.English
 
 
-const lanOptions = Object.values(Language).filter(x => { return !Number.isInteger(x) } )
+const lanOptions = Object.values(Language).filter(x => { return !Number.isInteger(x) })
 
 
 const save = () => {
@@ -17,33 +17,33 @@ const save = () => {
 
 
 
-export default function Header(){
-  
-  const [labels, updatelabels]  = useState(miselanea[lanSelected])
+export default function Header() {
 
-  const changeLanguage = (val:number) => {
+  const [labels, updatelabels] = useState(miselanea[lanSelected])
+
+  const changeLanguage = (val: number) => {
     lanSelected = Language[Language[val] as keyof typeof Language]
-  
+
     updatelabels(miselanea[lanSelected])
   }
-  
+
   return (
     <header id="header" className=" text-white bg-indigo-800 flex">
 
-    <span className="flex-1 text-3xl ">Gantt</span>
+      <span className="flex-1 text-3xl ">Gantt</span>
 
-    
+
       <span className="flex-1 text-right">
-        <select className="text-black"  defaultValue={lanSelected} onChange={(e) => { changeLanguage(parseInt(e.target.value))}}>
+        <select className="text-black" defaultValue={lanSelected} onChange={(e) => { changeLanguage(parseInt(e.target.value)) }}>
           {lanOptions.map((obj, index) => {
             return <option key={index} value={index}>{obj}</option>
           })}
         </select>
-        <button type="button" className="text-3xl  hover:brightness-110 px-5" onClick={() => {save()}}>
+        <button type="button" className="text-3xl  hover:brightness-110 px-5" onClick={() => { save() }}>
           💿
         </button>
       </span>
-        
-      </header>)
-  
-    }
+
+    </header>)
+
+}
